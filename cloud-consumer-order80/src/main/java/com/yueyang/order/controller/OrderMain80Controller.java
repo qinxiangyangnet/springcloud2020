85dd666b926aa8d92536c25857d0e9f6f6c0381c
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-04-13 22:05:38 +0800

    Ribbon随机IRule

--- cloud-consumer-order80/src/main/java/com/yueyang/order/controller/OrderMain80Controller.java
+++ cloud-consumer-order80/src/main/java/com/yueyang/order/controller/OrderMain80Controller.java
@@ -6,6 +6,8 @@ import com.netflix.loadbalancer.RoundRobinRule;
 import com.yueyang.springcloud.entities.CommonResult;
 import com.yueyang.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @program: springcloud2020
@@ -33,11 +36,14 @@ public class OrderMain80Controller {
     //public  static  final  String PAYMENT_URL="http://localhost:8001";
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
+    private DiscoveryClient discoveryClient;
+
     @Resource
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment) {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
 
 
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
@@ -60,8 +66,6 @@ public class OrderMain80Controller {
         }
 
     }
-
-
 
 
 }

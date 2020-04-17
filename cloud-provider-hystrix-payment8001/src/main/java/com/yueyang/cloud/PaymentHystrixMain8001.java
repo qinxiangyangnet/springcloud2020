9cd5232244ff262a8f5696fa033cb5ee72e59d54
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-04-18 07:36:45 +0800

    服务降级

--- cloud-provider-hystrix-payment8001/src/main/java/com/yueyang/cloud/PaymentHystrixMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yueyang/cloud/PaymentHystrixMain8001.java
@@ -2,16 +2,19 @@ package com.yueyang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @program: cloud-consumerconsul-order80
  * @description:
+ *  EnableCircuitBreaker:主启动类激活 回路的意思
  * @author: qinxiangyang
  * @create: 2020-04-15 06:36
  **/
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class PaymentHystrixMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentHystrixMain8001.class, args);
--- cloud-provider-hystrix-payment8001/src/main/java/com/yueyang/cloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/yueyang/cloud/service/PaymentService.java
@@ -1,8 +1,12 @@
 package com.yueyang.cloud.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.hystrix.exception.HystrixTimeoutException;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * @program: cloud-consumerconsul-order80
@@ -28,18 +32,30 @@ public class PaymentService {
 
     /**
      * 非正常访问
-     *
+     *划定红线
      * @param id
      * @return
      */
+    @HystrixCommand(fallbackMethod = "paymentInfo_TimenHandler",commandKey="paymentInfo_timeout",groupKey="PGroup",
+            threadPoolKey="paymentInfo_timeoutThread",commandProperties = {
+            @HystrixProperty(name = "metrics.rollingStats.timeInMilliseconds", value = "1440"),
+
+    })
     public String paymentInfo_timeout(Integer id) {
-        int timeNumber = 3;
+        int timeNumber = 5;
         try {
             TimeUnit.SECONDS.sleep(timeNumber);
         } catch (Exception e) {
 
         }
+       // int  age=10/0;
 
-        return "线程池" + Thread.currentThread().getName() + "paymentInfo_error,id" + id + "\t" + "o(╥﹏╥)o哼哼" + "h耗时：" + timeNumber;
+
+        return "线程池" + Thread.currentThread().getName() + "paymentInfo_error,id" + id + "\t" + "o(╥﹏╥)o哼哼" + "h耗时："+timeNumber ;
+    }
+    public String paymentInfo_TimenHandler(Integer id) {
+
+        //温馨友好提示
+        return "线程池" + Thread.currentThread().getName() + "paymentInfo_TimenHandler,id" + id + "\t" + "服务器在忙，请稍后尝试";
     }
 }

7ec57946613f5790f1119c7a499261ac9bf755ba
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-04-20 15:45:28 +0800

    Predicate断言的配置

---
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -0,0 +1,14 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @program: cloud-consumerconsul-order80
+ * @description:
+ * @author: qinxiangyang
+ * @create: 2020-04-20 15:26
+ **/
+public class T2 {
+    public static void main(String[] args) {
+        ZonedDateTime zonedDateTime =  ZonedDateTime.now();
+        System.out.println(zonedDateTime);
+    }
+}

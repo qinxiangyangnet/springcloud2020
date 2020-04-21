b01fdccd231b8376daa4f72164d45759c0baf2f9
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-04-22 04:56:04 +0800

    配置中心客户端，服务端开发

---
+++ cloud-config-client-3355/src/main/java/com/yueyang/cloud/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.yueyang.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-21 18:08
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${spring.datasource}")
+    private String configInfo;
+
+    @GetMapping("/datasource")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}

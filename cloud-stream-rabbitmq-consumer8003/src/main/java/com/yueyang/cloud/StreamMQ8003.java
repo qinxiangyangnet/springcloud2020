f93b90b87d376616a14a1c9ef2c2f1c9494fe1d2
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-04-25 00:34:05 +0800

    自定义分组及持久化

---
+++ cloud-stream-rabbitmq-consumer8003/src/main/java/com/yueyang/cloud/StreamMQ8003.java
@@ -0,0 +1,17 @@
+package com.yueyang.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: cloud-consumerconsul-order80
+ * @description:
+ * @author: qinxiangyang
+ * @create: 2020-04-24 23:29
+ **/
+@SpringBootApplication
+public class StreamMQ8003 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQ8003.class, args);
+    }
+}
